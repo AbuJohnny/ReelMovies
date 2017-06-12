183560c845b092a0115050a2682156013e9e19f6
Sarmad Georgie <sarmadmjg@gmail.com>
2017-06-12 18:00:32 +0300

    Change Movie member variable names to match fresh_tomatoes.py

--- entertainment_center/media.py
+++ entertainment_center/media.py
@@ -1,6 +1,6 @@
 class Movie():
 
-    def __init__ (self, title, poster_url, yt_trailer_url):
+    def __init__ (self, title, poster_image_url, trailer_youtube_url):
         self.title = title
-        self.poster_url = poster_url
-        self.yt_trailer_url = yt_trailer_url
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
