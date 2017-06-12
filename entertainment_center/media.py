18f63dc82b8891bd740000a5839968382d4301e9
Sarmad Georgie <sarmadmjg@gmail.com>
2017-06-12 16:10:44 +0300

    Create Movie class

---
+++ entertainment_center/media.py
@@ -0,0 +1,6 @@
+class Movie():
+
+    def __init__ (self, title, poster_url, yt_trailer_url):
+        self.title = title
+        self.poster_url = poster_url
+        self.yt_trailer_url = yt_trailer_url
